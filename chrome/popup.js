93f396f869f807901064550ab5fcfa98ac62c18d
Boschert <erich.boschert@fiserv.com>
2025-06-05 11:52:28 -0400

    adding options screen

--- chrome/popup.js
+++ chrome/popup.js
@@ -300,38 +300,6 @@ pushButton.addEventListener("click", async function () {
   try {
     let info = await getSingleDeviceInfo(); // this gets the active device when no pkey is supplied
     let transactions = (await buildRequest(info, "GET", "/push/v2/device/transactions")).response.transactions;
-    // let transactions = [
-    //   {
-    //     "access_device_ble_status": "unknown",
-    //     "akey": "****",
-    //     "attributes": [
-    //       [
-    //         [
-    //           "Organization",
-    //           "Oregon State University"
-    //         ],
-    //         [
-    //           "Integration",
-    //           "OSU Login"
-    //         ]
-    //       ],
-    //     ],
-    //     "block_biometric_pin_fallback": false,
-    //     "expiration": 1749059460,
-    //     "is_proximity_push": false,
-    //     "is_strict_proximity_push": false,
-    //     "phone_notification_type": "visible_notification",
-    //     "require_lock": false,
-    //     "require_touch_id": false,
-    //     "step_up_code_info": {
-    //       "num_digits": 3
-    //     },
-    //     "summary": "OSU Login",
-    //     "txid": "txIDss,",
-    //     "type": "Login",
-    //     "urgid": "urgidd"
-    //   }
-    // ];
     // If no transactions exist at the moment
     if (transactions.length == 0) {
       failedAttempts++;
@@ -744,183 +712,18 @@ document.getElementById("deleteButton").onclick = async () => {
   });
 };
 
-// Import button
-let dataSplash = document.getElementById("importSplash");
-document.getElementById("importButton").addEventListener("click", async function () {
-  // Warn if we're going to override devices
-  if ((await getDeviceInfo()).devices?.length > 0) {
-    showDeleteModal(`This will replace all data! Continue?`, () => {
-      document.getElementById("importFile").click();
-    });
+document.getElementById("openOptions").addEventListener("click", () => {
+  // https://developer.chrome.com/docs/extensions/develop/ui/options-page#linking
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
   } else {
-    document.getElementById("importFile").click();
+    window.open(chrome.runtime.getURL('options.html'));
   }
 });
-
-const importFile = document.getElementById("importFile");
-importFile.addEventListener("change", async (e) => {
-  const file = e.target.files[0];
-  if (!file) return;
-  // Get original (working) data
-  let ogData = await getExportableData();
-  const reader = new FileReader();
-  reader.onload = async (e) => {
-    console.log("Importing data");
-    try {
-      dataSplash.innerHTML = "Checking...";
-      // In case this is successful data, we need to clear all devices so old ones don't get retained
-      await clearAll();
-      // Set the data with new data. If it fails we simply fallback to ogData
-      await setDeviceInfo(JSON.parse(atob(e.target.result)), false);
-      // Get sanitized data
-      let newData = await getDeviceInfo();
-      // Ensure transactions for all devices still work
-      let invalidDevices = 0;
-      for (let index in newData.devices) {
-        let device = newData.devices[index];
-        dataSplash.innerHTML = `Verifying ${Number(index) + 1} / ${newData.devices.length}...`;
-        // Grab that data
-        // Wait for the device data to load
-        let singleDevice = await getSingleDeviceInfo(device);
-        try {
-          // Attempt to simply get the transactions
-          await buildRequest(singleDevice, "GET", "/push/v2/device/transactions");
-        } catch (e) {
-          console.error("Failed to verify device", e);
-          invalidDevices++;
-        }
-      }
-      // If none of the devices worked
-      if (invalidDevices == newData.devices.length) throw new Error("None of the devices passed validation");
-      // Success! Above code didn't throw errors
-      dataSplash.innerHTML = `Updating data...`;
-      await setDeviceInfo(newData);
-      if (invalidDevices > 0) {
-        dataSplash.innerHTML = `Data imported, but ${invalidDevices} device${invalidDevices == 1 ? "" : "s"} failed validation`;
-      } else {
-        dataSplash.innerHTML = "Data imported!";
-      }
-      // You can now populate fields or use the data as needed
-    } catch (err) {
-      console.error("Failed to verify data", err);
-      // If it failed, go back to OG data
-      setDeviceInfo(ogData).then(() => {
-        // Tell the user this is an invalid code
-        dataSplash.innerText = `Invalid data`;
-      }).catch(e => {
-        console.error("Failed to set back to original data");
-        // Tell the user this is an invalid code
-        dataSplash.innerText = `Failed to set back to old data. Reimport this later!`;
-        downloadFile(btoa(JSON.stringify(ogData)), "auto-2fa.txt");
-      });
-    } finally {
-      importFile.value = "";
-    }
-  };
-  reader.readAsText(file);
-});
-
-// Export button
-document.getElementById("exportButton").addEventListener("click", async function () {
-  // Needs to save a file
-  // Set text to be data. Scramble with Base64
-  if ((await getDeviceInfo()).devices?.length > 0) {
-    downloadFile(btoa(JSON.stringify(await getExportableData())), "auto-2fa.txt");
-    dataSplash.innerText = `Data exported!`;
-  } else {
-    dataSplash.innerText = `No data to export!`;
-  }
-});
-
-async function getExportableData() {
-  // Always will have data thanks to sanitization
-  let info = await getDeviceInfo();
-  // Get all device data by keys
-  let allDevices = await new Promise((resolve) => chrome.storage.sync.get(info.devices, resolve)); // so ig you can grab multiple storage values at once
-  info.devices = info.devices.map((key) => allDevices[key]);
-  return info;
-}
-
-// Export TOTPs
-document.getElementById("exportTOTPButton").addEventListener("click", async function () {
-  let info = await getDeviceInfo();
-  let allDevices = await new Promise((resolve) => chrome.storage.sync.get(info.devices, resolve));
-  let totps = [];
-  // Get all devices that have TOTP data
-  for (let index in allDevices) {
-    let device = allDevices[index];
-    if (device.hotp_secret) {
-      totps.push(totp.keyuri(device.name, "Duo Mobile", base32Encode(device.hotp_secret)));
-    }
-  }
-  if (!totps.length) {
-    dataSplash.innerHTML = "No devices have TOTP data!";
-  } else {
-    downloadFile(totps.join("\n"), "duo-mobile-totps.txt");
-    if (totps.length == info.devices.length) {
-      dataSplash.innerHTML = `Exported all devices`;
-    } else {
-      dataSplash.innerHTML = `Exported ${totps.length} of ${info.devices.length}`;
-    }
-  }
-});
-
-function base32Encode(input) {
-  const base32Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-  let binary = "";
-  for (let i = 0; i < input.length; i++) {
-    binary += input[i].charCodeAt(0).toString(2).padStart(8, "0");
-  }
-  let encoded = "";
-  for (let i = 0; i < binary.length; i += 5) {
-    const chunk = binary.slice(i, i + 5).padEnd(5, "0");
-    const index = parseInt(chunk, 2);
-    encoded += base32Alphabet[index];
-  }
-  // while (encoded.length % 8 !== 0) {
-  //   encoded += "=";
-  // }
-  return encoded;
-}
-
-// thank you authenticator <3
-function downloadFile(data, filename) {
-  const blob = new Blob([data], { type: "text/plain" });
-  // Simulate pressing <a> tag
-  const a = document.createElement("a");
-  a.href = URL.createObjectURL(blob);
-  a.download = filename;
-  a.click();
-  URL.revokeObjectURL(a.href);
-}
-
-// Reset button
-let resetSplash = document.getElementById("resetSplash");
-document.getElementById("resetButton").onclick = () => {
-  showDeleteModal(`Are you sure you want to delete all data?`, async () => {
-    await clearAll();
-    // Reset main page
-    slideIndex = 0;
-    errorSplash.innerText = "Use arrows to flip through instructions:";
-    activateButton.innerText = "Activate";
-    getDeviceInfo().then(updatePage);
-  });
-};
-
-// Delete the entire thing
-// We are not using local storage anymore, but it WAS being used in earlier versions
-// I also don't know what happens if the user doesn't have syncing enabled (i think just uses local)
-async function clearAll() {
-  await new Promise((resolve) => chrome.storage.session.clear(resolve));
-  await new Promise((resolve) => chrome.storage.sync.clear(resolve));
-  await new Promise((resolve) => chrome.storage.local.clear(resolve));
-}
 
 // Updates page information to new device information
 const deviceSettingsDiv = document.getElementById("deviceSettingsDiv");
 async function updatePage(deviceInfo) {
-  // Reset globals
-  dataSplash.innerHTML = "Manage data";
   // Remove devices already added
   Array.from(deviceSelect.options).forEach(option => {
     if (option.value !== "-1") deviceSelect.removeChild(option);
