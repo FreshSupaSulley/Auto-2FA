d6ee1d6b565c9345dc58d323bc52829ef64d2906
Erich Boschert <erich.boschert10@gmail.com>
2026-03-13 22:56:19 -0400

    bump

--- wxt.config.ts
+++ wxt.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     return {
       name: "Auto 2FA",
       description: "Login through Duo Mobile in your browser",
-      version: "1.6.2",
+      version: "1.6.3",
       permissions: ["storage", "activeTab", "scripting"],
       host_permissions: ["https://*.duosecurity.com/*"],
       // Specific to firefox during dev
